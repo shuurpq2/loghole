f4412bf88e3001fe146fe06e4910e7e9fd16b868
savethebees <afghanistaneurobolivar@gmail.com>
2026-06-16 15:11:51 +0300

    add log level colors

--- include/debug.h
+++ include/debug.h
@@ -4,7 +4,7 @@
 #include <iostream>
 
 #ifdef LH_DEBUG
-    #define LH_DEBUG_PRINT(msg) std::cout << "[LH_DEBUG] " << msg << "\n";
+    #define LH_DEBUG_PRINT(msg) std::cout << "[\033[1;36mLH_DEBUG\033[0m] " << msg << "\n";
 #else
     #define LH_DEBUG_PRINT(msg) do {} while (0)
 #endif
--- src/log_level.cpp
+++ src/log_level.cpp
@@ -2,11 +2,11 @@
 
 std::string log_level_to_string(LogLevel level) {
     switch(level) {
-        case LogLevel::DEBUG: return "DEBUG";
-        case LogLevel::INFO: return "INFO";
-        case LogLevel::WARNING: return "WARNING";
-        case LogLevel::ERROR: return "ERROR";
-        case LogLevel::FATAL: return "FATAL";
+        case LogLevel::DEBUG: return "\033[34mDEBUG\033[0m";
+        case LogLevel::INFO: return "\033[32mINFO\033[0m";
+        case LogLevel::WARNING: return "\033[33mWARNING\33[0m";
+        case LogLevel::ERROR: return "\033[31mERROR\033[0m";
+        case LogLevel::FATAL: return "\033[1;31mFATAL\033[0m";
         default: return "UNKNOWN";
     }
 }
